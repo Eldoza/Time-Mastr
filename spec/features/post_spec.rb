6e2d8b2f0e06dae89aacca055b407bc2c66ddedf
Chris Mendoza <cmend85@gmail.com>
2016-12-10 20:15:28 -0600

    Implemented create action for post and refactored

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'navigate' do
+
   describe 'index' do
     it 'can be reached successfully' do
       visit posts_path
@@ -12,4 +13,21 @@ describe 'navigate' do
       expect(page).to have_content(/Posts/)
     end
   end
+
+    describe 'creation' do
+      before do
+        visit new_post_path
+      end
+
+      it 'has a new form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be created from a new form page' do
+        fill_in 'post[date]', with: Date.today
+        fill_in 'post[rationale]', with: "Some rationale"
+        click_on "Save"
+        expect(page).to have_content("Some rationale")
+      end
+    end
 end
