f7136219121f24b79e392b581a432b97bae25ce8
Chris Mendoza <cmend85@gmail.com>
2016-12-11 20:54:09 -0600

    Refactored index to use post partial

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "anything")
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Chuck", last_name: "Norris")
+      @post = Post.create(date: Date.today, rationale: "anything", user_id: @user.id)
     end
 
     it "can be created" do
