3ed87f5a178cf3db01c1e345add5e004048f9e16
Chris Mendoza <cmend85@gmail.com>
2017-01-03 22:31:21 -0600

    Updated seeds file

--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation ' do
+    it 'can properly be created' do
+      
+    end
+
+  end
 end
