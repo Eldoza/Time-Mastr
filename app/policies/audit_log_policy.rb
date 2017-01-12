744b6a7152f69a2948b365e6bc33198e373f0add
Chris Mendoza <cmend85@gmail.com>
2017-01-11 18:21:19 -0600

    Built pending confirmation button functionality and tests

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -3,11 +3,11 @@ class AuditLogPolicy < ApplicationPolicy
     return true if admin?
   end
 
-  private
-
-  def user_or_admin?
-    record.user.id == user.id || admin?
+  def confirm?
+    record.user_id == user.id
   end
+
+  private
 
   def admin?
     admin_types.include?(user.type)
